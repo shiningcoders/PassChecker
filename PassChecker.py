815ca62f99045bdeb0bd8e38a9e95f14c1fd8a04
Prikshit Sharma <prikshit.sharma50@gmail.com>
2020-03-08 23:23:57 +0530

    added comments

--- PassChecker.py
+++ PassChecker.py
@@ -1,11 +1,13 @@
+# Enter password to check
 checkpass = input('Enter your Password :')
 count = 0
 
+# Loop to change password lists (It deployes one after another)
 for i in range(0, 8):
     fname = (f'{i}.prik')
     fhandle = open(fname)
 
-
+    #Check if password present in list
     for past in fhandle:
         past=past.strip()
         if past == checkpass:
@@ -15,6 +17,6 @@ for i in range(0, 8):
         else:
             print(past)
 
-
+# If password not present in lists
 if count == 0:
     print('All Good. ')
