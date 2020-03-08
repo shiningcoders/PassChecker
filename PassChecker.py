f89d6349f8005f1bc3329810c6444b25df62f2d7
unknown <ajayrajsinghab@gmail.com>
2020-03-08 21:47:56 +0530

    first commit

--- PassChecker.py
+++ PassChecker.py
@@ -11,7 +11,7 @@ for i in range(0, 8):
         if past == checkpass:
             print(past, 'isn\'t  hard to brute. ')
             count = count + 1
-            break
+            exit(0)
         else:
             print(past)
 
